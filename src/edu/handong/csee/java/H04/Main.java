6945b946318a008e8ded6ee93adc60ce507b240e
Chodabeen <82192889+Chodabeen@users.noreply.github.com>
2021-07-24 16:02:21 +0900

    Update Main.java

--- src/edu/handong/csee/java/H04/Main.java
+++ src/edu/handong/csee/java/H04/Main.java
@@ -3,7 +3,7 @@ package edu.handong.csee.java.H04;
 import java.util.Scanner;
 import java.util.Random;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
         Main stepH = new Main();
         stepH.h04();
